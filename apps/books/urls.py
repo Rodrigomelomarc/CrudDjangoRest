6c74d3fdfff9646c0b33344d3b7b8049da38f25e
Rodrigo de Melo Marcolino <rodrigomelomarcolino@gmail.com>
2019-05-14 00:22:51 -0300

    Adjust in books

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -4,14 +4,9 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('', csrf_exempt(views.BookView.as_view({
-        'get': 'list',
-        'post': 'create'
-    }))),
-    path('<int:pk>', csrf_exempt(views.BookView.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    })))
+    path('', views.ListBookView.as_view()),
+    path('create', csrf_exempt(views.CreateBookView.as_view())),
+    path('retrieve/<int:pk>', csrf_exempt(views.RetrieveBookView.as_view())),
+    path('update/<int:pk>', csrf_exempt(views.UpdateBookView.as_view())),
+    path('delete/<int:pk>', csrf_exempt(views.DeleteBookView.as_view()))
 ]
--- apps/books/views.py
+++ apps/books/views.py
@@ -1,9 +1,34 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import RetrieveAPIView, CreateAPIView,\
+    ListAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated, DjangoObjectPermissions
 
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookView(ModelViewSet):
+class CreateBookView(CreateAPIView):
+    serializer_class = BookSerializer
+    permission_classes = IsAuthenticated
+
+
+class RetrieveBookView(RetrieveAPIView):
+    serializer_class = BookSerializer
+    permission_classes = IsAuthenticated
+
+
+class ListBookView(ListAPIView):
+    serializer_class = BookSerializer
+    permission_classes = (AllowAny,)
+    queryset = Book.objects.all()
+
+
+class UpdateBookView(UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = DjangoObjectPermissions
+
+
+class DeleteBookView(DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = DjangoObjectPermissions
